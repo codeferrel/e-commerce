51ea3089834d796a3df244e23f3479e6f63ebbdf
codeferrel <anolestal@gmail.com>
2026-02-13 23:33:27 +0700

    add/code

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <h2>register</h2>
+  <form action="">
+    <input type="text" placeholder="nama">
+    <input type="text" placeholder="email">
+    <input type="text" placeholder="password">
+  </form>
+</body>
+</html>
