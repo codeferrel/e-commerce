dc95c83c26b2f8b39059f36869947b2a8f14422f
codeferrel <anolestal@gmail.com>
2026-02-23 23:16:38 +0700

    field

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,7 +17,7 @@
             margin: 0;
         }
 
-        /* Container utama form */
+       
         .register-container {
             background-color: #ffffff;
             padding: 2rem;
@@ -25,7 +25,6 @@
             box-shadow: 0 8px 24px rgba(0, 0, 0, 0.1);
             width: 100%;
             max-width: 400px;
-            text-align: center;
         }
 
         h2 {
